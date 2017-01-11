116a61ae17077db167522a6a526707ff16b110f7
javacasm <javacasm@gmail.com>
2017-01-11 14:35:04 +0100

    4 dispositivos

--- Ejemplos/esp8266_webserver_electricaldevices/esp8266_webserver_electricaldevices.ino
+++ Ejemplos/esp8266_webserver_electricaldevices/esp8266_webserver_electricaldevices.ino
@@ -1,11 +1,15 @@
 #include <ESP8266WiFi.h>
- 
+
+/*
+Ejemplo del instructable http://www.instructables.com/id/Quick-Start-to-Nodemcu-ESP8266-on-Arduino-IDE/?ALLSTEPS
+de Magesh Jayakunar
+*/
 const char* ssid = "Magesh";
 const char* password = "jayakumar";
- 
-; // 
+
+; //
 WiFiServer server(80);
- 
+
 void setup() {
   Serial.begin(115200);
   delay(10);
@@ -17,91 +21,91 @@ void setup() {
   digitalWrite(4, LOW);
   digitalWrite(0, LOW);
   digitalWrite(13, LOW);
- 
+
   // Connect to WiFi network
   Serial.println();
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
- 
+
   WiFi.begin(ssid, password);
- 
+
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
   Serial.println("");
   Serial.println("WiFi connected");
- 
+
   // Start the server
   server.begin();
   Serial.println("Server started");
- 
+
   // Print the IP address
   Serial.print("Use this URL to connect: ");
   Serial.print("http://");
   Serial.print(WiFi.localIP());
   Serial.println("/");
- 
+
 }
- 
+
 void loop() {
   // Check if a client has connected
   WiFiClient client = server.available();
   if (!client) {
     return;
   }
- 
+
   // Wait until the client sends some data
   Serial.println("new client");
   while(!client.available()){
     delay(1);
   }
- 
+
   // Read the first line of the request
   String request = client.readStringUntil('\r');
   Serial.println(request);
   client.flush();
- 
+
   // Match the request
- 
- 
+
+
   if (request.indexOf("/light1on") > 0)  {
     digitalWrite(5, HIGH);
-   
+
   }
   if (request.indexOf("/light1off") >0)  {
     digitalWrite(5, LOW);
-   
+
   }
 
    if (request.indexOf("/light2on") > 0)  {
     digitalWrite(4, HIGH);
-   
+
   }
   if (request.indexOf("/light2off") >0)  {
     digitalWrite(4, LOW);
-   
+
   }
     if (request.indexOf("/light3on") >0)  {
     digitalWrite(0, HIGH);
-   
+
   }
   if (request.indexOf("/light3off") > 0)  {
     digitalWrite(0, LOW);
-   
+
   }
    if (request.indexOf("/light4on") > 0)  {
     digitalWrite(13, HIGH);
-   
+
   }
   if (request.indexOf("/light4off") > 0)  {
     digitalWrite(13, LOW);
-   
+
   }
 // Set ledPin according to the request
 //digitalWrite(ledPin, value);
- 
+
   // Return the response
   client.println("HTTP/1.1 200 OK");
   client.println("Content-Type: text/html");
@@ -112,7 +116,7 @@ void loop() {
   client.println("<meta name='apple-mobile-web-app-capable' content='yes' />");
   client.println("<meta name='apple-mobile-web-app-status-bar-style' content='black-translucent' />");
  client.println("</head>");
-  client.println("<body bgcolor = \"#f7e6ec\">"); 
+  client.println("<body bgcolor = \"#f7e6ec\">");
   client.println("<hr/><hr>");
   client.println("<h4><center> Esp8266 Electrical Device Control </center></h4>");
   client.println("<hr/><hr>");
@@ -121,45 +125,45 @@ void loop() {
   client.println("<center>");
   client.println("Device 1");
   client.println("<a href=\"/light1on\"\"><button>Turn On </button></a>");
-  client.println("<a href=\"/light1off\"\"><button>Turn Off </button></a><br />");  
-  client.println("</center>");   
+  client.println("<a href=\"/light1off\"\"><button>Turn Off </button></a><br />");
+  client.println("</center>");
   client.println("<br><br>");
    client.println("<center>");
    client.println("Device 2");
   client.println("<a href=\"/light2on\"\"><button>Turn On </button></a>");
-  client.println("<a href=\"/light2off\"\"><button>Turn Off </button></a><br />");  
-client.println("</center>"); 
+  client.println("<a href=\"/light2off\"\"><button>Turn Off </button></a><br />");
+client.println("</center>");
   client.println("<br><br>");
     client.println("<center>");
    client.println("Device 3");
   client.println("<a href=\"/light3on\"\"><button>Turn On </button></a>");
-  client.println("<a href=\"/light3off\"\"><button>Turn Off </button></a><br />");  
-client.println("</center>"); 
+  client.println("<a href=\"/light3off\"\"><button>Turn Off </button></a><br />");
+client.println("</center>");
   client.println("<br><br>");
    client.println("<center>");
    client.println("Device 4");
   client.println("<a href=\"/light4on\"\"><button>Turn On </button></a>");
-  client.println("<a href=\"/light4off\"\"><button>Turn Off </button></a><br />");  
-client.println("</center>"); 
+  client.println("<a href=\"/light4off\"\"><button>Turn Off </button></a><br />");
+client.println("</center>");
   client.println("<br><br>");
   client.println("<center>");
   client.println("<table border=\"5\">");
  client.println("<tr>");
   if (digitalRead(5))
-         { 
+         {
            client.print("<td>Light 1 is ON</td>");
-        
+
          }
           else
           {
             client.print("<td>Light 1 is OFF</td>");
-      
+
         }
-     
+
         client.println("<br />");
-             
+
          if (digitalRead(4))
-          { 
+          {
            client.print("<td>Light 2 is ON</td>");
 
          }
@@ -176,7 +180,7 @@ client.println("</center>");
 
           if (digitalRead(0))
 
-          { 
+          {
            client.print("<td>Light 3 is ON</td>");
 
           }
@@ -191,7 +195,7 @@ client.println("</center>");
           if (digitalRead(13))
 
 
-          { 
+          {
 
 
            client.print("<td>Light 4 is ON</td>");
@@ -216,9 +220,9 @@ client.println("</center>");
           client.println("</table>");
 
           client.println("</center>");
-  client.println("</html>"); 
+  client.println("</html>");
   delay(1);
   Serial.println("Client disonnected");
   Serial.println("");
- 
+
 }
